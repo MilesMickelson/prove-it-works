144726529347a4b726fbd38402f1e1f399ed037a
Miles Mickelson <milesmickelson87@gmail.com>
2020-01-20 11:51:30 -0800

    refactors code from original project completion

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 const Mortgage = require('../../src/js/lib/Mortgage');
 
 export default class App extends React.Component {
@@ -12,22 +13,22 @@ export default class App extends React.Component {
       monthlyPayment: 0
     };
     this.handleChange = this.handleChange.bind(this);
-    this.calculateMonthlyPayment = this.calculateMonthlyPayment.bind(this)
+    this.calculateMonthlyPayment = this.calculateMonthlyPayment.bind(this);
   }
 
   handleChange(event) {
     switch (event.target.name) {
       case 'principal':
-        this.setState({ principal: Number(event.target.value) })
+        this.setState({ principal: Number(event.target.value) });
         break;
       case 'loanTerm':
-        this.setState({ loanTerm: Number(event.target.value) })
+        this.setState({ loanTerm: Number(event.target.value) });
         break;
       case 'period':
-        this.setState({ period: Number(event.target.value) })
+        this.setState({ period: Number(event.target.value) });
         break;
       case 'interestRate':
-        this.setState({ interestRate: Number(event.target.value) })
+        this.setState({ interestRate: Number(event.target.value) });
         break;
       default:
         break;
@@ -35,24 +36,24 @@ export default class App extends React.Component {
   }
 
   calculateMonthlyPayment() {
-    let mortgage = new Mortgage(
+    const mortgage = new Mortgage(
       this.state.principal,
       this.state.interestRate,
       this.state.loanTerm,
-      this.state.period)
-    let monthlyPayment = mortgage.monthlyPayment()
-    let monthly = document.getElementById('output')
-    monthly.innerText = "$" + monthlyPayment
+      this.state.period);
+    const monthlyPayment = mortgage.monthlyPayment();
+    const monthly = document.getElementById('output');
+    monthly.innerText = '$' + monthlyPayment;
   }
 
   render() {
     return (
       <div className='App'>
         <h1>Mortgage Calculator</h1>
-        <input onChange={this.handleChange} name='principal' />
-        <input onChange={this.handleChange} name='interestRate' />
-        <input onChange={this.handleChange} name='loanTerm' />
-        <select onChange={this.handleChange} name='period'>
+        <input onChange={ this.handleChange } name='principal' />
+        <input onChange={ this.handleChange } name='interestRate' />
+        <input onChange={ this.handleChange } name='loanTerm' />
+        <select onChange={ this.handleChange } name='period'>
           <option value='12'>Monthly</option>
           <option value='4'>Quarterly</option>
         </select>
